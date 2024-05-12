b14ba3518a9ea30b73ce7bacbff1083e7fe0e49c
X0re4ik <xoore4ik@gmail.com>
2024-05-12 13:55:42 +0500

    очень простое приложенеи для тестов

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 import os
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
+
+hooks_results = [
+]
+
+@app.route("/", methods = ['GET', 'POST', 'DELETE'])
+def main():
+    global hooks_results
+    if request.method == 'POST':
+        hooks_results.append(request.json)    
+    return {
+        "q": "Pochemy na nebe tak mnogo zvezd?",
+        "q1": "Mi kazahi",
+        "hooks": hooks_results,
+    }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8055)
